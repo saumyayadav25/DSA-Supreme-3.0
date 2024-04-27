e53039e6b92b993187ae4fc2a2a65f6ccfd1ebc3
saumyayadav25 <141031443+saumyayadav25@users.noreply.github.com>
2024-04-27 16:26:04 +0530

    Create variablesAndDatatypes.cpp

---
+++ Introduction to C++/variablesAndDatatypes.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // declaration
+    // int age;
+    // cout<<age<<endl;
+    // definition
+    int age=25;
+    age=101;
+    cout<<age<<endl;
+
+    // int
+    int count=5;
+    //float
+    float share=3.14;
+    //char
+    char alphabet='z'; //always use single quote for defining character
+    //double
+    double weight=55.69889;
+    //boolean
+    bool isMale= false;
+    bool isChild=1;
+    bool isFemale= true;
+    bool isAdult=0;
+    cout<<count<<endl;
+    cout<<share<<endl;
+    cout<<alphabet<<endl;
+    cout<<weight<<endl;
+    cout<<isMale<<endl;
+    int marks=12;
+    cout<< sizeof(marks)<<endl;
+    return 0;
+}
