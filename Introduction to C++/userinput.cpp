b7eab4763a8670063a464085276aeaccd8ce705a
saumyayadav25 <141031443+saumyayadav25@users.noreply.github.com>
2024-04-27 16:35:38 +0530

    Create userinput.cpp

---
+++ Introduction to C++/userinput.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cin>>age;
+    cout<<"your age is:"<<age<<endl;
+    float marks;
+    cout<<"enter your marsk: "<<endl;
+    cin>>marks;
+    cout<<marks<<endl;
+    char grade;
+    cout<<"enter your grade: "<<endl;
+    cin>>grade;
+    cout<<"your grade is"<<grade<<endl;
+    return 0;
+}
