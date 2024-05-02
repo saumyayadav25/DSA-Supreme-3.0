3a7fb3c56c40eae67b2addeab15f2999a9db43a0
saumyayadav25 <141031443+saumyayadav25@users.noreply.github.com>
2024-05-02 22:16:45 +0530

    Create variableAndDatatypes.cpp

---
+++ Basics of Programming-level-1/variableAndDatatypes.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<4<<5<<6;
+    cout<<endl;
+    //variable declaration
+    int age;
+    cout<<"enter your age"<<endl;
+    cin>>age;
+    cout<<age;
+    //variables and datatypes
+    //variable initialisation
+    int marks=20; //assign value 20 to age.
+    float f=12.08;
+    cout<<sizeof(f)<<endl;
+    //out of range : char: -128 to 127
+    char ch=128;
+    cout<<ch<<endl;
+    short value=32770;
+    cout<<value<<endl;
+    cout<<sizeof(value)<<endl;
+    return 0;
+}
