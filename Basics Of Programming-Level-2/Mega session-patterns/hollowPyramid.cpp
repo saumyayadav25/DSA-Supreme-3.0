08f7e1419cf54cb41b00afb2e2aadb7a3b69f427
saumyayadav25 <141031443+saumyayadav25@users.noreply.github.com>
2024-05-16 20:00:25 +0530

    Create hollowPyramid.cpp

---
+++ Basics Of Programming-Level-2/Mega session-patterns/hollowPyramid.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void hollowPyramid(int n){
+    for(int row=0;row<n;row++){
+        // spaces
+        for(int col=0;col<n-row-1;col++){
+            cout<<" ";
+        }
+        //stars
+        for(int col=0; col<row+1;col++){
+            if(row==0||row==n-1||col==0||col==row ){
+                cout<<"* ";
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    hollowPyramid(n);
+    return 0;
+}
