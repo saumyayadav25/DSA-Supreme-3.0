41761ebbd960ed9129168b486e409b87ec6a4578
saumyayadav25 <141031443+saumyayadav25@users.noreply.github.com>
2024-05-17 12:14:39 +0530

    Create callStack.cpp

---
+++ Basics Of Programming-Level-2/Bitwise Operators and Functions/callStack.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+//call stack
+void hello(){
+    cout<<4;
+    return;
+}
+void solve(){
+    cout<<3;
+    hello();
+    cout<<4;
+}
+int main(){
+    
+    cout<<1;
+    solve();
+    cout<<2;
+    return 0;
+    return 0;
+}
