2d00d6cf4c415c3d1f658bcfa94092c644e7c633
saumyayadav25 <141031443+saumyayadav25@users.noreply.github.com>
2024-05-21 10:56:17 +0530

    Create upperTriangleIncludingDiagonal.cpp

---
+++ Arrays, Time complexity and Space complexity/Array class-2/All pairs/upperTriangleIncludingDiagonal.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+//print all pairs
+void printAllPairs(int arr[],int n){
+    for(int i=0; i<n;i++){
+        //for every i, we are running looping variable "j" from start to end
+        for(int j=0;j<=i;j++){
+            cout<<arr[i]<<", "<<arr[j]<<endl;
+        }
+    }
+}
+int main(){
+    int arr[]={10,20,30,40};
+    int size=4;
+    printAllPairs(arr,size);
+    return 0;
+}
