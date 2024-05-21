86a31ef3c3ed57bbdff6a363ae44f5f5749e55b6
saumyayadav25 <141031443+saumyayadav25@users.noreply.github.com>
2024-05-21 11:02:34 +0530

    Update lowerTriangleWithoutDiagonal.cpp

--- Arrays, Time complexity and Space complexity/Array class-2/All pairs/lowerTriangleWithoutDiagonal.cpp
+++ Arrays, Time complexity and Space complexity/Array class-2/All pairs/lowerTriangleWithoutDiagonal.cpp
@@ -4,9 +4,14 @@ using namespace std;
 void printAllPairs(int arr[],int n){
     for(int i=0; i<n;i++){
         //for every i, we are running looping variable "j" from start to end
-        for(int j=n-1;j>i;j--){
+        //ethod-1
+        for(int j=n-1;j>i;j--){ 
             cout<<arr[i]<<", "<<arr[j]<<endl;
         }
+        //method-2
+        // for(int j=i+1;j<n;j++){
+        //     cout<<arr[i]<<", "<<arr[j]<<endl;
+        // }
     }
 }
 int main(){
