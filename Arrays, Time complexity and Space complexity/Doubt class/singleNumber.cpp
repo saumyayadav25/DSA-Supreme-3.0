073527d98597d5018a4db111c9a57d1baad32ea5
saumyayadav25 <141031443+saumyayadav25@users.noreply.github.com>
2024-05-25 21:29:59 +0530

    Create singleNumber.cpp

---
+++ Arrays, Time complexity and Space complexity/Doubt class/singleNumber.cpp
@@ -0,0 +1,21 @@
+//single number( alternative approach of XOR method)
+class Solution{
+public:
+    int sigleNumber(vector<int>& nums){
+        //sorting
+        sort(nums.begin(), nums.end()); 
+        int n=nums.size();
+        int ans=-1;
+        int i=0;
+        while(i < n){
+            if(i + 1 < n && nums[i]==nums[i+1]){
+                i +=2;
+            }
+            else{
+                ans =nums[i];
+                break;
+            }
+        }
+        return ans;
+    }
+};
