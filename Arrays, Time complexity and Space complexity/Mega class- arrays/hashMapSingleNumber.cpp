2b4c22a9c6644ad758b1143580ccbbcf8e001128
saumyayadav25 <141031443+saumyayadav25@users.noreply.github.com>
2024-05-26 16:14:17 +0530

    Create hashMapSingleNumber.cpp

---
+++ Arrays, Time complexity and Space complexity/Mega class- arrays/hashMapSingleNumber.cpp
@@ -0,0 +1,25 @@
+//single number
+//leetcode 136: single number
+class Solution{
+public:
+    int singleNumber(vector<int>&nums){
+        //frequency table
+        unordered_map<int,int>freqMap;
+        for(int i=0; i<nums.size();i++){
+            int num=nums[i];
+            freqMap[num]=freqMap[num]+1;
+        }
+        unordered_map<int,int>::iterator it;
+        int ans;
+        for(it = freqMap.begin(); it!=freqMap.end(); it++){
+            int key= it->first;
+            int freq= it->second;
+            if(freq==1){
+                ans=key;
+                break;
+            }
+            // cout<<key<<" "<<freq<<endl;
+        }
+        return ans;
+    }
+};
