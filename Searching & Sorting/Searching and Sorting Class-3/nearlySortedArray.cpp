b0fd7f090271cafe48aa4e2091d0a30b91d26d08
saumyayadav25 <141031443+saumyayadav25@users.noreply.github.com>
2024-05-30 21:34:15 +0530

    Create nearlySortedArray.cpp

---
+++ Searching & Sorting/Searching and Sorting Class-3/nearlySortedArray.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+//given a sorted array arr[] of size N, some elements of array are moved to either of the adjacent positions, i.e., arr[i] may be present at arr[i+1] or arr[i-1] . The task is to search for an element in this array
+//input: arr[]={10,3,40,20,50,80,70}, key=40
+//output: 2
+//explanation: output is index of 40 in given array
+
+//input: arr[]={10,3,40,20,50,80,70}, key=90
+//output: -1
+//explanation: -1 is returned to indicate element is not present
+int applyBinarySearch(int arr[], int size, int target){
+    int s=0;
+    int e=size-1;
+    int mid=s+(e-s)/2;
+    while(s<=e){
+        if(arr[mid]==target){
+            return mid;
+        }
+        if(arr[mid-1]==target){
+            return mid-1;
+        }
+        if(arr[mid+1]==target){
+            return mid+1;
+        }
+        if(target>arr[mid]){
+            //left
+            e=mid-2;
+        }
+        else{
+            //right
+            s=mid+2;
+        }
+        mid=s+(e-s)/2;
+    }
+    return -1;
+}
+int main(){
+    int arr[]={10,3,40,20,50,80,70};
+    int size=7;
+    int target=40;
+    int ans = applyBinarySearch(arr,size,target);
+    cout<<"found at index: "<<ans<<endl;
+    return 0;
+}
