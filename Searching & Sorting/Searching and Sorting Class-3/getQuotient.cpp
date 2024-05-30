819cdcd7959c43a6f840859be9b5a9dab4b84454
saumyayadav25 <141031443+saumyayadav25@users.noreply.github.com>
2024-05-30 21:01:17 +0530

    Create getQuotient.cpp

---
+++ Searching & Sorting/Searching and Sorting Class-3/getQuotient.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+//given two integers one is a dividend and the other is the divisor, we need to find the quotient when the dividend is divided by the divisor without the use of any "/" and "%" operators
+//for example
+//input: divident=10, divisor=2
+//output:5
+//explanation : 10.2 =5
+
+int getQuotient(int dividend , int divisor){
+    //search space: (-ve dividend)->(+ve dividend)
+    int s=0;
+    int e=dividend;
+    int mid = s + ((e-s) >> 1);
+    int ans=-1;
+    while(s<=e){
+        // cout<<"printing mid: "<<mid<<endl;
+        if((divisor * mid) == dividend){
+            return mid;
+        }
+        if((divisor * mid) < dividend){
+            //store and compute
+            ans = mid;
+            s=mid+1;
+        }
+        else{
+            e=mid-1;
+        }
+        mid = s + ((e-s) >> 1);
+    }
+    return ans;
+}
+int main(){
+    int dividend=10;
+    int divisor=-2;
+
+    int ans = getQuotient(abs(dividend),abs(divisor));
+    if((dividend<0 && divisor>0) || (dividend>0 && divisor<0)){
+        ans=0-ans;
+    }
+    cout<<"final ans: "<<ans<<endl;
+}
