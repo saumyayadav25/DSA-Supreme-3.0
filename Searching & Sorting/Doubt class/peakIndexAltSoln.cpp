9f903d974da150dc41ae676f35084bde5216c45c
saumyayadav25 <141031443+saumyayadav25@users.noreply.github.com>
2024-06-01 21:57:50 +0530

    Create peakIndexAltSoln.cpp

---
+++ Searching & Sorting/Doubt class/peakIndexAltSoln.cpp
@@ -0,0 +1,25 @@
+//leetcode 852: peak index in a mountain array
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int ans=-1;
+        int start=0;
+        int end= arr.size() -1;
+        while(start<=end){
+            int mid = (start + end)>>1;
+            if(arr[mid]< arr[mid+1]){
+                //peak at right side
+                start=mid+1;
+            }
+            else if(arr[mid] < arr[mid-1]){
+                //peak at left side
+                end = mid-1;
+            }
+            else{
+                ans=mid;
+                break;
+            }
+        }
+        return ans;
+    }
+};
