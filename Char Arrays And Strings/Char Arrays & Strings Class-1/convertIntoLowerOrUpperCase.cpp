3ef1f0e56d89c57dd291e408ea360bb36bddf476
saumyayadav25 <141031443+saumyayadav25@users.noreply.github.com>
2024-06-03 21:32:46 +0530

    Create convertIntoLowerOrUpperCase.cpp

---
+++ Char Arrays And Strings/Char Arrays & Strings Class-1/convertIntoLowerOrUpperCase.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+int getLength(char arr[], int size){
+    int count =0;
+    int index = 0;
+    while(arr[index]!= '\0'){
+        //till null character is not found
+        count++;
+        index++;
+    }
+    return count;
+}
+void convertIntoUpperCase(char arr[], int n){
+    int len = getLength(arr,n);
+    for(int i=0; i<len; i++){
+        char ch = arr[i];
+        //if letter if lowercase
+        if(ch>='a' && ch<='z'){
+            ch= ch-'a'+'A';
+        }
+        
+        arr[i]=ch;
+    }
+}
+void convertIntoLowerCase(char arr[], int n){
+    int len = getLength(arr,n);
+    for(int i=0; i<len; i++){
+        char ch = arr[i];
+        //if letter if lowercase
+        if(ch>='A' && ch<='Z'){
+            ch= ch-'A' + 'a';
+        }
+        
+        arr[i]=ch;
+    }
+}
+int main(){
+    char arr[1000];
+    cin>>arr;
+    convertIntoUpperCase(arr,1000);
+    cout<<arr<<endl;
+    convertIntoLowerCase(arr,1000);
+    cout<<arr<<endl;
+    return 0;
+}
