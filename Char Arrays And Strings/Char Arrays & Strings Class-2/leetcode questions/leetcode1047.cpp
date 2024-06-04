3bcbcac1f6ddbdfb0d4968328c6bcacda48ad77e
saumyayadav25 <141031443+saumyayadav25@users.noreply.github.com>
2024-06-04 21:24:34 +0530

    Create leetcode1047.cpp

---
+++ Char Arrays And Strings/Char Arrays & Strings Class-2/leetcode questions/leetcode1047.cpp
@@ -0,0 +1,21 @@
+//leetcode 1047: remove all adjacent duplicates in String
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string ans = "";
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            char currcharacter = s[i];
+            if(ans.empty()){
+                ans.push_back(currcharacter);
+            }
+            else if(currcharacter == ans.back()){ //rightmost character
+                ans.pop_back();
+            }
+            else if(currcharacter != ans.back()){
+                ans.push_back(currcharacter);         
+            }
+        }
+        return ans;
+    }
+};
