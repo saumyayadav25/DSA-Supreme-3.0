8c2b35ece00cda86e88540ddd9a751350f10a9ff
Saumya Yadav <141031443+saumyayadav25@users.noreply.github.com>
2024-07-22 22:22:24 +0530

    Add files via upload

---
+++ 12 Linked List/Week 10-LinkedList-Assignments/02_MergeTwoSortedLists.cpp
@@ -0,0 +1,47 @@
+// leetcode 21: Merge Two Sorted Lists
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == NULL) return list2;
+        if(list2 == NULL) return list1;
+
+        ListNode* ans = new ListNode(-1);
+        ListNode* mptr = ans;
+
+        while(list1 && list2){
+            if(list1->val <= list2->val){
+                mptr->next = list1;
+                mptr = list1;
+                list1 = list1->next;
+            }
+            else{
+                mptr->next = list2;
+                mptr = list2;
+                list2 = list2->next;
+            }
+        }
+        // leftover list
+        if(list1){
+            mptr->next = list1;
+            // mptr = list1;
+            // list1 = list1->next;
+        }
+        if(list2){
+            mptr->next = list2;
+            // mptr = list2;
+            // list2 = list2->next;
+        }
+        return ans->next;
+    }
+};
