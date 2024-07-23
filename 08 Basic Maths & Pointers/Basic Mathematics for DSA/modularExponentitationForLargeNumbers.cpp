e4956e703f49dca033c10b0c7ac0db3ee0d6567d
Saumya Yadav <141031443+saumyayadav25@users.noreply.github.com>
2024-07-23 17:28:49 +0530

    Update modularExponentitationForLargeNumbers.cpp

--- 08 Basic Maths & Pointers/Basic Mathematics for DSA/modularExponentitationForLargeNumbers.cpp
+++ 08 Basic Maths & Pointers/Basic Mathematics for DSA/modularExponentitationForLargeNumbers.cpp
@@ -11,7 +11,7 @@ class Solution
 		long long int PowMod(long long int x,long long int n,long long int M)
 		{
             long long int ans = 1;
-            while(n>=0){
+            while(n>0){
                 if(n&1) ans = (ans * x) % M;
                 x = (x * x) % M;
                 n>>=1;
