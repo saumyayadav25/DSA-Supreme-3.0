5666c1e1bebcf9311c5e4cd5bf97af6b057887ae
Saumya Yadav <141031443+saumyayadav25@users.noreply.github.com>
2024-08-28 22:21:35 +0530

    Update 01-create-Heap.cpp

--- 17 Heaps/Heaps Class - 1/01-create-Heap.cpp
+++ 17 Heaps/Heaps Class - 1/01-create-Heap.cpp
@@ -2,162 +2,114 @@
 using namespace std;
 
 class Heap{
-    public:
-        int *arr;
-        int capacity; // total number of elements that heap can hold
-        int size; // current number of elements inside heap
-        Heap(int capacity){
-            this->capacity = capacity;
-            arr = new int[capacity];
-            size = 0;
-        }
-        // TC : O(logn)
-        void insert(int val){
-            // STEP 1: insert the element normally into the first empty position inside the array
-            // STEP 2: then compare the element with the parent and put the element at the right place
-            // also make sure that you are handling the overflow case
-            if(size==capacity){
-                cout<<"OVERFLOW<<"<<endl;
-                return;
-            }
-            // it means new element ke liye space hai
-            // STEP 1: 
-            size++:
-            int index = size;
-            arr[index] = val;
-            // STEP 2: HEAPIFY
-            while(index>1){
-                int parentIndex = index/2;
-                if(arr[index]> arr[parentIndex]){
-                    swap(arr[index],arr[parentIndex]);
-                    index = parentIndex;
-                }
-                else{
-                    break;
-                }
-            }
-        }
-        
-        // TC : O(logn)
-        int deleteFromHeap(){
-            int answer = arr[1];
-            // replace
-            swap(arr[1],arr[size]);
-            // remove
-            size--;
-            // updated root node is at index 1 now
-            int index = 1;
-            while(index < size){
-                int i = index;
-                int parent = i;
-                int left = 2 * i;
-                int right = 2 * i + 1;
+public:
+    int *arr;
+    int capacity;
+    int index;
 
-                // let's find out sbse bada element
-                int largestElementIndex = parent;
-                if(left <= size && arr[largestElementIndex] < arr[left]){
-                    largestElementIndex = left;
-                }
-                if(right<= size && arr[largestElementIndex] < arr[right]){
-                    largestElementIndex = right;
-                }
-                // check whether we found a new largest element or not
-                if(largestElementIndex == parent){
-                    // no change-> parent hi largest hai: kuch nhi krna iska mtlb
-                    break;
-                }
-                else{
-                    // we found a new largestElement other than parent
-                    swap(arr[parent], arr[largestElementIndex]);
-                    index = largestElementIndex;
-                }
-            }
-            return answer;
-        }
-        // another way to delete
+    Heap(int n){
+        this->capacity = n;
+        arr = new int[n];
+        index = 0;
+        // jab bhi insert krna hoga, tab index+1 krke insert krdena
+    }
 
-        int deleteFromHeap(){
-            int answer = arr[1];
-            // replace
-            swap(arr[1],arr[size]);
-            size--;
-            int i = 1;
-            while(i<size){
-                int currentIndex = i;
-                int leftChildIndex = 2*i;
-                int rightChildIndex = 2*i+1;
-
-                int currVal = arr[current];
-                int leftVal = arr[leftChildIndex];
-                int rightVal = arr[rightChildIndex];
-
-                int sabseBadaElementKaIndex = current;
-                if(currVal > leftVal && currVal >rightVal){
-                    sabseBadaElementKaIndex = currentIndex;
-                }
-                if(leftVal > currVal && leftVal >rightVal){
-                    sabseBadaElementKaIndex = leftChildIndex;
-                }
-                if(rightVal > currVal && rightVal >leftVal){
-                    sabseBadaElementKaIndex = rightChildIndex;
-                }
-                if(sabseBadaElementKaIndex == currentIndex){
-                    break;
-                }
-                else{
-                    swap(arr[currentIndex], arr[sabseBadaElementKaIndex]);
-                    i = sabseBadaElementKaIndex; //nhi kroge toh infinite loop me fas jaoge
-                }
-            }
-            return answer;
+    void printHeap(){
+        for(int i = 1; i<capacity;i++){
+            cout<<arr[i]<<" ";
         }
-        
-        void printHeap(){
-            for(int i =0; i<capacity;i++){
-                cout << arr[i]<<" ";
-            }
-            cout<<endl;
+        cout<<endl;
+    }
+
+    // TC : O(logn)
+    void insert(int val){
+        // STEP 1: insert the element normally into the first empty position inside the array
+        // STEP 2: then compare the element with the parent and put the element at the right place
+        if(index == capacity - 1){
+            cout << "overflow"<<endl;
+            return;
         }
+        // space is available
+        // STEP 1: 
+        index++;
+        arr[index] = val;
+        // now put value at right position-> by comparing with parent node
+        int i = index; 
+        // STEP 2: HEAPIFY
+        while(i > 1){ //index ki jgh i pe kaam krenge ab
+            int parentIndex = i/2;
+            if(arr[parentIndex] < arr[i]){
+                swap(arr[parentIndex],arr[i]);
+                i = parentIndex;
+            }
+            else{ 
+                //parent ki value already badi hai, no need to do anything
+                break;
+            }
+        }
+    }
+
+    // TC : O(logn)
+    void deleteFromHeap(){
+        // replace
+        swap(arr[1],arr[index]);
+        // size decrease
+        index--;
+        // heapify
+        heapify(arr,index,1);
+    }
+
+    void heapify(int *arr, int n, int currIndex){
+         // n -> number of elements in heaps
+        int i = currIndex;
+        int leftIndex = 2 * i;
+        int rightIndex = 2 * i + 1;
+
+        int largestKaIndex = i; // assuming i par hi sbse badi value hai
+        // but it is possible ki sabse badi value left ya right me ho
+        // check for left,
+        if(leftIndex < n && arr[leftIndex] > arr[largestKaIndex]){ //compare with largestKaIndex and not i, kyuki ho skta hai i 10 ho, left me 40 ho and right me 30, toh jab right me compare krenge toh 30 ajayega i me jabki 40 hona chahiye tha
+            // left me badi value milgyi
+            largestKaIndex = leftIndex;
+        }
+        // check for right,
+        if(rightIndex < n && arr[rightIndex] > arr[largestKaIndex]){
+            // right me badi value milgyi
+            largestKaIndex = rightIndex;
+        }
+        // now we have sabse bade element ka index
+        // largestValue is still i, no need to do anything
+        // largestValue left ya right me se koi hai then swap
+        if(largestKaIndex != i){
+            // left ya right me se koi bada element milgya
+            swap(arr[i],arr[largestKaIndex]);
+            i = largestKaIndex; 
+            // baaki recursion sambhal lega
+            heapify(arr,n,i);
+        }
+    }
 };
 
-
-// aapke pass koi element hai 'index' par, usko right position par place krna hai[downwards]
-void heapify(int *arr, int n, int index){
-    int currentIndex = index;
-    int leftIndex = 2*index;
-    int rightIndex = 2*index + 1;
-
-    int currVal = arr[currentIndex];
-    int leftVal = arr[leftIndex];
-    int rightVal = arr[rightIndex];
-
-    int sabseBadaElementKaIndex = currentIndex;
-
-    if(currVal > leftVal && currVal >rightVal){
-        sabseBadaElementKaIndex = currentIndex;
-    }
-    if(leftVal > currVal && leftVal >rightVal){
-        sabseBadaElementKaIndex = leftIndex;
-    }
-    if(rightVal > currVal && rightVal >leftVal){
-        sabseBadaElementKaIndex = rightIndex;
-    }
-    if(sabseBadaElementKaIndex != currentIndex){
-        swap(arr[currentIndex], arr[sabseBadaElementKaIndex]);
-        currentIndex = sabseBadaElementKaIndex;
-        // heapify(arr,n,sabseBadaElementKaIndex);
-        heapify(arr,n,current);
-    }
-}
-
-void buildHeap(int){
-    // n/2-> 1 index
-    for(int i = n/2; i>0;i--){
-        heapify(arr,n,i);
-    }
-}
-
 int main(){
+    Heap pq(10);
+    // 50 40 20 10 30
+    pq.insert(10);
+    pq.printHeap();
+    pq.insert(20);
+    pq.printHeap();
+    pq.insert(30);
+    pq.printHeap();
+    pq.insert(40);
+    pq.printHeap();
+    pq.insert(50);
+    pq.printHeap();
+
+    pq.deleteFromHeap();
+    pq.printHeap();
+    pq.deleteFromHeap();
+    pq.printHeap();
+    pq.deleteFromHeap();
+    pq.printHeap();
 
     return 0;
 }
