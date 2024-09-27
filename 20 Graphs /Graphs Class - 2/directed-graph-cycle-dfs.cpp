5d71b246aedb0aceed72d374737efc5570564dde
Saumya Yadav <141031443+saumyayadav25@users.noreply.github.com>
2024-09-27 10:00:50 +0530

    Update directed-graph-cycle-dfs.cpp

--- 20 Graphs /Graphs Class - 2/directed-graph-cycle-dfs.cpp
+++ 20 Graphs /Graphs Class - 2/directed-graph-cycle-dfs.cpp
@@ -2,35 +2,41 @@
 
 class Solution {
   public:
-    bool DFSHelper(int node, unordered_map<int,bool>&vis, vector<int>adj[],unordered_map<int,bool>&dfsTracker){
+    bool dfsHelper(int node, unordered_map<int,bool>& vis, vector<int> adj[],
+    unordered_map<int,bool>& dfsTracker) {
+        
         vis[node] = true;
         dfsTracker[node] = true;
-
-        for(auto nbr: adj[node]){
-            if(!vis[nbr]){
-                bool ans = DFSHelper(nbr,vis,adj,dfsTracker);
-                if(ans==true) return true;
+        
+        for(auto nbr: adj[node]) {
+            if(!vis[nbr]) {
+                bool ans = dfsHelper(nbr, vis, adj, dfsTracker);
+                if(ans == true) {
+                    return true;
+                }
             }
-            else if(vis[nbr] == true && dfsTracker[nbr] == true){
-                // cycle present
+            else if(vis[nbr] == true && dfsTracker[nbr] == true) {
+                //cycle present
                 return true;
             }
         }
-        // backtrack
+        
+        //backtrack
         dfsTracker[node] = false;
         return false;
     }
-
-    // Function to detect cycle in a directed graph.
     bool isCyclic(int V, vector<int> adj[]) {
         bool ans = false;
         int n = V;
-        unordered_map<int,bool>vis;
-        unordered_map<int,bool>dfsTracker;
-        for(int i = 0; i<n;i++){
-            if(!vis[i]){
-                ans = DFSHelper(i,vis,adj,dfsTracker);
-                if(ans == true) break;
+        unordered_map<int,bool> vis;
+        unordered_map<int,bool> dfsTracker;
+        
+        for(int i=0; i<n; i++) {
+            if(!vis[i]) {
+                ans = dfsHelper(i,vis,adj, dfsTracker);
+                if(ans == true) {
+                    break;
+                }
             }
         }
         return ans;
