fec400fcc280f3139d0ac4351e20d5eb0172fa23
Saumya Yadav <141031443+saumyayadav25@users.noreply.github.com>
2024-09-30 17:14:33 +0530

    iterative approach update

--- 09 Recursion/Week-7 Assignment/07_BestTimeToBuyAndSellStock.cpp
+++ 09 Recursion/Week-7 Assignment/07_BestTimeToBuyAndSellStock.cpp
@@ -1,4 +1,6 @@
 // leetcode 121: Best Time to Buy and Sell Stock
+
+// recursive
 class Solution {
 public:
     void maxProfitFinder(vector<int>& prices, int i, int &minPrice, int&maxProfit){
@@ -16,5 +18,24 @@ public:
         int maxProfit = INT_MIN;
         maxProfitFinder(prices,0,minPrice, maxProfit);
         return maxProfit;
+    }
+};
+
+
+
+// iterative approach: TC:o(N)
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max_profit = 0;
+        int minPrice = INT_MAX;
+        for(int i = 0; i < prices.size() ; i++){
+            if(prices[i]<minPrice){
+                minPrice = prices[i];
+            }
+            int todayProfit = prices[i] - minPrice;
+            max_profit = max(todayProfit, max_profit);
+        }
+        return max_profit;
     }
 };
