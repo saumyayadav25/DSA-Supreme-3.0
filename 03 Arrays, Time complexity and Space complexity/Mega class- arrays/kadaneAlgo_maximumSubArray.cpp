a0a4e07c4718fce458e73cbbf99457a09fc75a58
Saumya Yadav <141031443+saumyayadav25@users.noreply.github.com>
2024-09-30 16:41:03 +0530

    brute force maximum subarray

--- 03 Arrays, Time complexity and Space complexity/Mega class- arrays/kadaneAlgo_maximumSubArray.cpp
+++ 03 Arrays, Time complexity and Space complexity/Mega class- arrays/kadaneAlgo_maximumSubArray.cpp
@@ -1,4 +1,5 @@
 //leetcode 53: maximum subarray
+// KADANE ALGORITHM: TC:O(N)
 class Solution {
 public:
     int kadaneAlgo(vector<int>& nums){
@@ -14,5 +15,24 @@ public:
     }
     int maxSubArray(vector<int>& nums) {
         return kadaneAlgo(nums);
+    }
+};
+
+
+
+// BRUTE FORCE: TC:O(N^2)->TLE
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int ans = INT_MIN;
+        int n = nums.size();
+        for(int i = 0; i<n;i++){
+            int sum = 0;
+            for(int j = i; j<n;j++){
+                sum+=nums[j];
+                ans = max(ans, sum);
+            }
+        }
+        return ans;
     }
 };
