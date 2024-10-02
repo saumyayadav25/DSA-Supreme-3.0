36943d0d02ae9bb99a8ef6f9b06a4462ba89157a
Saumya Yadav <141031443+saumyayadav25@users.noreply.github.com>
2024-10-02 21:17:27 +0530

    binary search approach update

--- 19 Dynamic Programming Level - 1/DP class - 2/02-longest-increasing-subsequence.cpp
+++ 19 Dynamic Programming Level - 1/DP class - 2/02-longest-increasing-subsequence.cpp
@@ -72,3 +72,28 @@ public:
         return solveTab(nums,curr,prev);
     }
 };
+
+
+
+// optimised solution : BINARY SEARCH->mega class
+// TC: O(nlogn)
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int>ans;
+        ans.push_back(nums[0]);
+
+        for(int i = 1; i<nums.size();i++){ // O(N)
+            if(nums[i] > ans.back()){
+                ans.push_back(nums[i]);
+            }
+            else{
+                // find the index of just >= the ith element(lowerbound->gives iterator not index) 
+                auto it = lower_bound(ans.begin(),ans.end(),nums[i]); //O(logn)
+                int index = it - ans.begin(); //get index from iterator
+                ans[index] = nums[i];
+            }
+        }
+        return ans.size();
+    }
+};
