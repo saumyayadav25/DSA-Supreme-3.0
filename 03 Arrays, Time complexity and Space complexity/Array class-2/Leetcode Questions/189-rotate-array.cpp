5d77a78de0799d8400157ac2abb2436f7848a811
Saumya Yadav <141031443+saumyayadav25@users.noreply.github.com>
2024-10-17 10:48:59 +0530

    Create 189-rotate-array.cpp

---
+++ 03 Arrays, Time complexity and Space complexity/Array class-2/Leetcode Questions/189-rotate-array.cpp
@@ -0,0 +1,15 @@
+// leetcode 189. Rotate array
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%n;
+        //step 1. reverse all
+        reverse(nums.begin(), nums.end());
+        //step 2. reverse first k
+        reverse(nums.begin(), nums.begin()+k);
+        //step 3. reverse first n-k
+        reverse(nums.begin()+k, nums.end());
+    }
+};
