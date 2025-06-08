f2726b7b50ed42f49ab73e5b958318a030b316fc
Saumya Yadav <141031443+saumyayadav25@users.noreply.github.com>
2025-06-08 11:34:57 +0530

    gfg Print adjacency list sol added

--- 20 Graphs/Graphs Class - 1/main.cpp
+++ 20 Graphs/Graphs Class - 1/main.cpp
@@ -52,3 +52,21 @@ int main(){
     g.printAdjList(n);
     return 0;
 }
+
+
+
+// gfg "Print adjacency list" solution
+class Solution {
+  public:
+    // Function to return the adjacency list for each vertex.
+    vector<vector<int>> printGraph(int V, vector<pair<int, int>>& edges) {
+        vector<vector<int>>ans(V);
+        for(auto edge: edges){
+            int u = edge.first;
+            int v = edge.second;
+            ans[u].push_back(v);
+            ans[v].push_back(u);
+        }
+        return ans;
+    }
+};
